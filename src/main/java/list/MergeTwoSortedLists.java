fdaa8a3db97ac53cd621e60b16ff1cfb56dd99c2
Sayantan Khan <iamstk14@gmail.com>
2024-04-02 22:21:19 +0530

    Leetcode 21: Merge Two Sorted List

--- src/main/java/list/MergeTwoSortedLists.java
+++ src/main/java/list/MergeTwoSortedLists.java
@@ -16,7 +16,7 @@ public class MergeTwoSortedLists {
     public static void mergerTwoList(List<Integer> l1, List<Integer> l2) {
         // Option1 : Iterate through both the lists and put them into another list
         // and do a merge sort on that list
-        // if size(l1) = m, size(l2) = n; Time Complexity = O(mn log mn); Space = O(mn)
+        // if size(l1) = m, size(l2) = n; Time Complexity = O(m+n log m+n); Space = O(m+n)
         watch.start();
         mergeTwoListBruteForce(l1, l2);
         watch.stop();
@@ -24,10 +24,19 @@ public class MergeTwoSortedLists {
 
         watch.reset();
 
-        // Option2 : As the lists are sorted; we can use a min heap and store the elements from
+        // Option2 : As the lists are sorted; we can take element and compare and put in the list
+        // if size(l1) = m, size(l2) = n; Time Complexity = O(m+n);
+        watch.start();
+        mergeTwoListWithoutSortingAlgo(l1, l2);
+        watch.stop();
+        System.out.println("Time taken for mergeTwoListWithoutSortingAlgo "+ watch.getNanoTime());
+
+        watch.reset();
+
+        // Option3 : As the lists are sorted; we can use a min heap and store the elements from
         // each iteration and heap will give us the min
         // if size(l1) = m, size(l2) = n; no of sorted list -> 2;
-        // Time Complexity = O(mn log2) -> O(mn)
+        // Time Complexity = O(m+n log2) -> O(m+n)
         watch.start();
         mergeTwoListMinHeap(l1, l2);
         watch.stop();
@@ -68,12 +77,36 @@ public class MergeTwoSortedLists {
         }
     }
 
+    private static void mergeTwoListWithoutSortingAlgo(List<Integer> l1, List<Integer> l2) {
+
+        List<Integer> list = new ArrayList<>();
+        int i = 0, j = 0;
+        while(i < l1.size() && j < l2.size()) {
+            if(l1.get(i) < l2.get(j)) {
+                list.add(l1.get(i));
+                i++;
+            } else {
+                list.add(l2.get(j));
+                j++;
+            }
+        }
+
+        while(i < l1.size()) {
+            list.add(l1.get(i));
+            i++;
+        }
+        while(j < l2.size()) {
+            list.add(l1.get(j));
+            j++;
+        }
+    }
+
     private static void mergeTwoListBruteForce(List<Integer> l1, List<Integer> l2) {
         List<Integer> list = new ArrayList<>();
         Iterator i1 = l1.iterator();
         Iterator i2 = l2.iterator();
 
-        // O(m*n)
+        // O(m+n)
         while(i1.hasNext() || i2.hasNext()) {
             if(i1.hasNext()) list.add((Integer) i1.next());
             if(i2.hasNext()) list.add((Integer) i2.next());
@@ -84,8 +117,8 @@ public class MergeTwoSortedLists {
     }
 
     public static void main(String[] args) {
-        List<Integer> even = IntStream.range(1, 1000).filter(x -> x%2 == 0).boxed().collect(Collectors.toList());
-        List<Integer> odd = IntStream.range(1, 1000).filter(x -> x%2 != 0).boxed().collect(Collectors.toList());
+        List<Integer> even = IntStream.range(1, 100000).filter(x -> x%2 == 0).boxed().collect(Collectors.toList());
+        List<Integer> odd = IntStream.range(1, 100000).filter(x -> x%2 != 0).boxed().collect(Collectors.toList());
 
         mergerTwoList( odd, even);
     }
