72c8a7355166517d9b0dffc8b11247fbac9a00d1
Sayantan Khan <iamstk14@gmail.com>
2024-04-07 17:53:15 +0530

    Leetcode 124: Binary Tree Max Path Sum

---
+++ src/main/java/tree/BinaryTreeMaximumPathSum.java
@@ -0,0 +1,38 @@
+package tree;
+
+import util.Utility;
+
+// https://leetcode.com/problems/binary-tree-maximum-path-sum/description/
+public class BinaryTreeMaximumPathSum {
+
+    static int max = Integer.MIN_VALUE;
+    public static int maxPathSumRec(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = maxPathSumRec(root.left);
+        int right = maxPathSumRec(root.right);
+
+        int local = root.val + left + right; // U
+        // If one child is neg : keep max child
+        int temp = root.val + Math.max(left, right);
+        // If both the children are neg : only root
+        int negTemp = root.val;
+
+        // This to calculate global maxima
+        max = Math.max(max, Math.max(local, Math.max(temp, negTemp)));
+
+        // returning local maxima
+        return Math.max(temp, negTemp);
+    }
+
+    public static int maxPathSum(TreeNode root) {
+        maxPathSumRec(root);
+        return max;
+    }
+
+    public static void main(String[] args) throws Exception {
+        TreeNode root = Utility.generateBinartTreeFromArray(new Integer[]{-10,9,20,null,null,15,7});
+        Utility.assertTrue(maxPathSum(root), 42);
+    }
+
+}
--- src/main/java/util/Utility.java
+++ src/main/java/util/Utility.java
@@ -1,6 +1,10 @@
 package util;
 
+import tree.TreeNode;
+
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Utility {
 
@@ -11,4 +15,40 @@ public class Utility {
     public static void assertTrue(int[] actual, int[] expected) throws Exception {
         if(!Arrays.equals(actual, expected)) throw new Exception("Actual: "+actual+" Expected: "+expected);
     }
+
+    public static TreeNode generateBinartTreeFromArray(Integer[] integers) {
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        int height = 0;
+        TreeNode root = new TreeNode(integers[0]);
+        queue.add(root);
+
+        int index = 1;
+        while(!queue.isEmpty()) {
+            if(index >= integers.length) {
+                break;
+            }
+
+            TreeNode node = queue.poll();
+
+            Integer left = integers[index];
+            if(left != null) {
+                node.left = new TreeNode(left);
+                queue.add(node.left);
+            }
+
+
+            Integer right = integers[index+1];
+            if(right != null) {
+                node.right = new TreeNode(right);
+                queue.add(node.right);
+            }
+
+            height++;
+            index += 2;
+        }
+
+        return root;
+    }
+
 }
