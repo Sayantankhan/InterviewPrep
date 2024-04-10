7633c5a985bd2b911a5e1eae9cf3ccb7e71e411e
Sayantan Khan <iamstk14@gmail.com>
2024-04-10 12:47:16 +0530

    Benchmark Profile Update

--- src/main/java/custom/CustomNonDupList.java
+++ src/main/java/custom/CustomNonDupList.java
@@ -1,2 +1,32 @@
-package custom;public class CustomNonDupList {
+package custom;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+/*
+ * Create a class
+ *  > Insert an element (Non duplicate)
+ *  > Removing from any position
+ *  > Generate random value present in the string with equal probablity
+ */
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Benchmark)
+@Fork(value = 1)
+@Warmup(iterations = 5, timeUnit = TimeUnit.MILLISECONDS, time = 5000)
+@Measurement(iterations = 5, timeUnit = TimeUnit.MILLISECONDS, time = 5000)
+public class CustomNonDupList {
+
+    @Benchmark
+    public void init() {
+        int[] arr = new int[10];
+        Arrays.fill(arr, -1);
+    }
 }
