68d24f0b3d7f845e1e3dcbbb3004c254fe563ff9
Sayantan Khan <iamstk14@gmail.com>
2024-04-16 17:00:19 +0530

    Leetcode 1498: Number of Subsequences That Satisfy the Given Sum Condition

--- src/main/java/arrays/twopointers/NumberofSubsequencesThatSatisfyGivenSumCondition.java
+++ src/main/java/arrays/twopointers/NumberofSubsequencesThatSatisfyGivenSumCondition.java
@@ -1,2 +1,35 @@
-package arrays.twopointers;public class NumberofSubsequencesThatSatisfyGivenSumCondition {
+package arrays.twopointers;
+
+import util.Utility;
+
+import java.util.Arrays;
+import java.util.Map;
+
+//https://leetcode.com/problems/number-of-subsequences-that-satisfy-the-given-sum-condition/description/
+public class NumberofSubsequencesThatSatisfyGivenSumCondition {
+
+    public static int numSubseq(int[] nums, int target) {
+        Arrays.sort(nums);
+        int start = 0;
+        int end = nums.length - 1;
+
+        int ans = 0;
+        int mod = 1000000007;
+
+        while (start <= end) {
+            if (nums[start] + nums[end] > target) {
+                end --;
+            } else {
+                ans += (int) (Math.pow(2, end - start)) % mod;
+                start++;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) throws Exception {
+//       Utility.assertTrue(numSubseq(new int[]{3,5,6,7}, 9), 4);
+//       Utility.assertTrue(numSubseq(new int[]{3,3,6,8}, 10), 6);
+         Utility.assertTrue(numSubseq(new int[]{2,3,3,4,6,7}, 12), 61);
+    }
 }
