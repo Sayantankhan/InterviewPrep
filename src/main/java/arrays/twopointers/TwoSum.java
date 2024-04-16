8804d4eb89d3c27f81057c648fed5414beea2f29
Sayantan Khan <iamstk14@gmail.com>
2024-04-16 16:55:38 +0530

    Leetcode 167: Two Sum II - Input Array Is Sorted

--- src/main/java/arrays/twopointers/TwoSum.java
+++ src/main/java/arrays/twopointers/TwoSum.java
@@ -1,2 +1,29 @@
-package arrays.twopointers;public class TwoSum {
+package arrays.twopointers;
+
+import util.Utility;
+
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+public class TwoSum {
+
+    public static int[] twoSum(int[] numbers, int target) {
+        // As the array is sorted , we dont need to sort
+
+        int start = 0, end = numbers.length - 1;
+        while (start < end) {
+            if(numbers[start] + numbers[end] == target)  {
+                return new int[]{start+1, end+1};
+            } else if(numbers[start] + numbers[end] > target) {
+                end --;
+            } else {
+                start++;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Utility.assertTrue(twoSum(new int[]{2,7,11,15}, 9), new int[]{1, 2});
+        Utility.assertTrue(twoSum(new int[]{2,3,4}, 6), new int[]{1, 3});
+        Utility.assertTrue(twoSum(new int[]{-1,0}, -1), new int[]{1, 2});
+    }
 }
