219a42829bb5adb5eab3fe96442d010ee359f619
Sayantan Khan <iamstk14@gmail.com>
2024-05-03 13:25:49 +0530

    Leetcode 132: Pallindrome Partitioning II
    - Front Partition

--- src/main/java/dp/PalindromePartitioningII.java
+++ src/main/java/dp/PalindromePartitioningII.java
@@ -1,9 +1,11 @@
 package dp;
 
+import java.util.stream.IntStream;
+
 // https://leetcode.com/problems/palindrome-partitioning-ii/description/
 public class PalindromePartitioningII {
 
-    public static int minCut(String s) {
+    public static int minCutDP(String s) {
         // Front partition
       // return minCutRec(s, 0) - 1;
 
@@ -22,6 +24,30 @@ public class PalindromePartitioningII {
         }
 
         return dp[0] - 1;
+    }
+
+    public static int minCut(String s) {
+        // validate input
+        if (s == null || s.length() <= 1) {
+            return 0;
+        }
+        // dp
+        int N = s.length();
+        int[] dp = IntStream.range(0, N).toArray(); // initial value: dp[i] = i
+
+        for (int mid = 1; mid <  N; mid++) { // iterate through all chars as mid point of palindrome
+            // CASE 1. odd len: center is at index mid, expand on both sides
+            for (int start = mid, end = mid; start >= 0 && end < N && s.charAt(start) == s.charAt(end); start--, end++) {
+                int newCutAtEnd = (start == 0) ? 0 : dp[start - 1] + 1;
+                dp[end] = Math.min(dp[end], newCutAtEnd);
+            }
+            // CASE 2: even len: center is between [mid-1,mid], expand on both sides
+            for (int start = mid - 1, end = mid; start >= 0 && end < N && s.charAt(start) == s.charAt(end); start--, end++) {
+                int newCutAtEnd = (start == 0) ? 0 : dp[start - 1] + 1;
+                dp[end] = Math.min(dp[end], newCutAtEnd);
+            }
+        }
+        return dp[N - 1];
     }
 
 
@@ -47,9 +73,9 @@ public class PalindromePartitioningII {
     }
 
     public static void main(String[] args) {
-        System.out.println(minCut("sbb"));
+
+        System.out.println(minCut("abb"));
+        System.out.println(minCut("bbbbb"));
         System.out.println(minCut("ababababababababababababcbabababababababababababa"));
-        System.out.println(isPallindrom("ababababababababababababcbabababababababababababa"));
-        System.out.println(minCut("aaabaa"));
     }
 }
