abb3ceb04cd27e87d14ccf0ebe34276313a7a7ab
Sayantan Khan <iamstk14@gmail.com>
2024-09-28 15:16:07 +0530

    Update TopKFrequentElements.java

--- src/main/java/arrays/TopKFrequentElements.java
+++ src/main/java/arrays/TopKFrequentElements.java
@@ -6,6 +6,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 // https://leetcode.com/problems/top-k-frequent-elements/description/
+// Top K Frequent Elements
 public class TopKFrequentElements {
     /*
      * Given an integer array nums and an integer k, return the k most frequent elements.
@@ -30,6 +31,45 @@ public class TopKFrequentElements {
             res[i] = list.get(i).getKey();
         }
         return res;
+    }
+
+
+    // Approach 2: Priority Queue
+    class Tuple {
+        int number;
+        int count;
+
+        Tuple(int number, int count) {
+            this.number = number;
+            this.count = count;
+        }
+    }
+
+    public int[] topKFrequentpq(int[] nums, int k) {
+        Arrays.sort(nums);
+        int[] res = new int[k];
+
+        PriorityQueue<Tuple> pq = new PriorityQueue<Tuple>((t1, t2) -> {
+            return t2.count - t1.count;
+        });
+
+        for(int i = 0; i < nums.length; i++) {
+            int count = 0;
+            int n = nums[i];
+            while(i+1 < nums.length && nums[i] == nums[i+1]) {
+                count++;
+                i++;
+            }
+            pq.offer(new Tuple(n, count+1));
+        }
+        int index = 0;
+        while(k-1 >= 0) {
+            res[index++] = pq.poll().number;
+            k--;
+        }
+
+        return res;
+
     }
 
     public static void main(String[] args) throws Exception {
